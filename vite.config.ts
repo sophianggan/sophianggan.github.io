3e3f60f9558083c95c6248eeb3e477a5b8deafee
Sophia Ng Gan <sophianggan@Sophias-MacBook-Air-13.local>
2025-11-15 23:21:55 -0500

    Update base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/website/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
